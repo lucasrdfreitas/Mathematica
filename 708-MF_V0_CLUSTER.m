dce88cda9294c8dc8b75035272d632b39cebcf0c
lucasrdfreitas <lucasrdf@fisica.ufrn.br>
2023-06-17 03:42:43 +0200

    2

--- 708-MF_V0_CLUSTER.m
+++ 708-MF_V0_CLUSTER.m
@@ -16,8 +16,8 @@ Print["Starting Kernels"];
 
 NbName="708"; \[Lambda]0=0.5; 
 
-		Ls = Range[40,40,4]; 				tV={0,1,1.5};				
-		hV=With[{h=0.2 ,\[CurlyPhi]=0},{ {h,0,\[CurlyPhi]} (*,{h,15,\[CurlyPhi]},{h,30,\[CurlyPhi]},{h,45,\[CurlyPhi]},{h,60,\[CurlyPhi]},{h,75,\[CurlyPhi]},{h,90,\[CurlyPhi]} *)
+		Ls = Range[36,36,4]; 				tV={-1,-1.5};				
+		hV=With[{h=0.01,\[CurlyPhi]=0},{ {h,0,\[CurlyPhi]} (*,{h,15,\[CurlyPhi]},{h,30,\[CurlyPhi]},{h,45,\[CurlyPhi]},{h,60,\[CurlyPhi]},{h,75,\[CurlyPhi]},{h,90,\[CurlyPhi]} *)
 		(*,{0.2612,45,45},{0.2612,45,90},{0.2612,90,0},{0.2612,90,45}*)   }   ];
 
 		steps=50;				acuracy=4;     \[CapitalDelta]ev=0.099999; eVs=Table[1700 x, {x,0,.65,\[CapitalDelta]ev}][[1;;-1]];  (* eV=\[Xi](U-3JH)=1500\[Xi] *)
