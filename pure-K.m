9a0472d69b3405033b8a6356eb7ccdf998784bec
lucasrdfreitas <lucasrdf@fisica.ufrn.br>
2023-07-13 15:31:36 +0200

    pure Quadrupole |h| dependence 2

--- pure-K.m
+++ pure-K.m
@@ -22,8 +22,10 @@ NbName="709";
 		\[Lambda]0=0.5; 
 		Ls = Range[42,42,2]; 				tV={0};		
 		\[CurlyPhi]V = {0}(*Union[ Table[\[CurlyPhi],{\[CurlyPhi],124,120+60,1}], Table[\[CurlyPhi],{\[CurlyPhi],124+180,120+180+60,1}]  ]*);
-		\[Theta]V = Table[\[Theta],{\[Theta],0,180,1}];		
-		hV = Flatten[Table[{h,\[Theta],120},{h,{.05,.15,.25,.35}},{\[Theta],\[Theta]V}],1]; 
+		\[Theta]V = Table[\[Theta],{\[Theta],120,140,1}];	
+\[Theta]V={0,60,115,120,125,180}	;
+h0V={.001,.1,.2,.3,.4};
+		hV = Flatten[Table[{h,\[Theta],120},{h,h0V},{\[Theta],\[Theta]V}],1]; 
 
 		steps=500;				acuracy=9;     eVs=Table[1700 x, {x,0,0,0.0499999}];  (* eV=\[Xi](U-3JH)=1500\[Xi] *)
 
