05a144d125a1525d132e2edcb9f1b9ccd840bd65
lucasrdfreitas <lucasrdf@fisica.ufrn.br>
2023-09-11 11:44:59 -0300

    804 cluster commit

---
+++ script800.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#$ -l hostname=micro4.local 
+#$ -N 800-LAG
+#$ -l h_rt=120:00:00                   #estimate max run time
+#$ -q all.q
+#$ -m ea
+#$ -M lucasrdf@fisica.ufrn.br
+#$ -cwd
+#$ -o /home/rodrigues/Mathematica2/.o
+#$ -e /home/rodrigues/Mathematica2/.e
+#
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/sge/lib/lx-amd64
+export CLASSPATH=/opt/sge/lib/drmaa.jar:/opt/sge/lib/juti.jar:/opt/sge/lib/jgdi.jar
+#
+math -script 804-CLUSTER.m
